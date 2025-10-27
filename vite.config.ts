908af4dc6c279285091d28f90b9381eabb51a8ac
BarrySang <barrysang8@gmail.com>
2025-10-27 23:24:52 +0300

    change project name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/react_tic-tac-toe/"
-  base: "https://barrysang.github.io/react_tic-tac-toe",
+  base: "/react_tic-tac-toe/",
+  // base: "https://barrysang.github.io/react_tic-tac-toe",
 });
